829522c9bc0ebbe30372a42fff65a8e13017e7f5
Aaron Robinson <aaron@phasebit.com>
2017-02-10 20:24:39 -0600

    created UserComponent and moved code from app module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>
-  <p><strong>Email:</strong> {{email}}</p>
-  <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+  template: `
+    <user></user>
   `,
 })
 export class AppComponent {
-  name = 'Aaron';
-  email = "aaron@gmail.com";
-  address = {
-    street: '12 main street',
-    city: 'Bentonville',
-    state: 'AR'
-  }
 }
