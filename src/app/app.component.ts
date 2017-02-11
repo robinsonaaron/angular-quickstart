6400e8562bd5a22e8cbeacff4f9f68eb5739e54b
Aaron Robinson <aaron@phasebit.com>
2017-02-10 20:21:56 -0600

    changes to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+  <p><strong>Email:</strong> {{email}}</p>
+  <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  name = 'Aaron';
+  email = "aaron@gmail.com";
+  address = {
+    street: '12 main street',
+    city: 'Bentonville',
+    state: 'AR'
+  }
+}
